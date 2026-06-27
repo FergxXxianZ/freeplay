6aecdb5c75d50ce66a5fca10ae5fc0d91141474a
snaigref <45558857+FergxXxianZ@users.noreply.github.com>
2026-06-27 12:21:40 +0700

    Update VideoPage.tsx

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -196,6 +196,7 @@ export const VideoPage: React.FC = () => {
     'https://cdn.videy.co/',
     'https://cdn2.videy.co/',
     'https://videy.co/', 
+    'https://vildey.com/', 
     'https://cdn.aceimg.com/', 
   ];
 
